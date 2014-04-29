a8f3bcda9d6ea3227d70c1f95fcfd780d525fc61
ichou <me@ichou.cn>
2014-04-29 12:21:41 +0800

    简化登录

--- helpers/sign.rb
+++ helpers/sign.rb
@@ -3,44 +3,54 @@ require 'rubygems'
 require 'mechanize'
 require 'rufus-scheduler'
 
-@s = Rufus::Scheduler.new
+@a = Mechanize.new
+# @s = Rufus::Scheduler.new
+# @l = Logger.new('log.txt')
+
+@a.user_agent_alias = 'Mac Safari'
+@a.request_headers = {'X-Requested-With' => 'XMLHttpRequest'}
+# @l.formatter = proc do |severity, datetime, progname, msg|
+#   "#{datetime}:#{msg}\n"
+# end
+
 @users = [
-  {'id' => 'me@ichou.cn', 'pwd' => '6542198', 'time' => '19:58:13'}
+  {:id => '某虫', :cookie => 'ichou.yaml'}
 ]
 
-@users.each { |user|
-  @s.every '1m', :first_at => user['time'] do
-    puts Time.now
-    sign(user)
-    puts "=================================\n"
-  end
-}
+# @users.each { |user|
+#   @s.every '1d', :first => '2014-04-27 00:13:00'  do
+#     sign(user)
+#   end
+# }
 
 def sign (user)
-  a = Mechanize.new 
-  a.user_agent_alias = 'Mac Safari'
-  
   # Rythem 抓包测试代理
-  #a.set_proxy('127.0.0.1', '8889')
+  # @a.set_proxy('127.0.0.1', '8889')
   
-  # login_page
-  login_page = a.get('https://login.xiami.com/member/login')
-  login_page.form.email = user['id']
-  login_page.form.password = user['pwd']
-  a.submit(login_page.form, login_page.form.buttons.first)
-  
+  # login
+  tmp = @a.get('http://www.xiami.com/index/home')
+  if tmp.body.length < 200
+    puts 'no login!'
+    @a.cookie_jar.load(user[:cookie])
+    @a.get('http://www.xiami.com/index/home')
+    @a.get("http://www.xiami.com/index/home?_=#{Time.now.to_i}")
+  end
+
   # signin
-  a.request_headers = {'X-Requested-With' => 'XMLHttpRequest'}
-  a.get("http://www.xiami.com/index/home?_=#{Time.now.to_i}")
-  my_page = a.post('http://www.xiami.com/task/signin')
-  a.request_headers = {'X-Requested-With' => nil}
+  puts 'okay, login!'
+  my_page = @a.post('http://www.xiami.com/task/signin')
 
   # logout
-  logout_page = a.get('http://www.xiami.com/member/logout')
+  # logout_page = @a.get('http://www.xiami.com/member/logout')
 
   # puts info
-  puts "Today is the #{my_page.body} day under signed of #{user['id']}"
-  
+  # @l.info("Today is the #{my_page.body} day under signed of #{user[:id]}")
+  puts "Today is the #{my_page.body} day under signed of #{user[:id]}"
+
 end
 
-@s.join
+sign(@users[0])
+sleep 5
+sign(@users[0])
+
+# @s.join
