de0725dabe7d5335d9dfe8e6a9632b7a9dafc1e7
ichou <me@ichou.cn>
2014-05-29 04:17:30 +0800

    Use bundle to manage gems

--- xiami_fm.rb
+++ xiami_fm.rb
@@ -2,9 +2,9 @@
 require_relative 'application'
 
 # 用户的 member_auth， 可从虾米网页的 cookie 中获取
-MemberAuth = "1WuYGtxL6mFvhfPCRI9kIXUY4rbTHTeBx40BirYk5AMkcYwJNteswauVRQtJ0SeVkY6wtGU1Rg"
+MemberAuth = #ur_member_auth_code#
 # 是否开启 320K
-HdSwitch = "on"
+HdSwitch = "off"
 
 trap('INT') { puts "\nClosing" ; exit }
 
